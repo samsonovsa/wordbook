c256decead83cf97e9467d3f0f1b45e739870b8e
Sergey Samsonov <samsonovsa@yandex.ru>
2020-02-08 17:14:08 +0300

    Add method GetConnectionString

---
+++ Config/Configuration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WordBook.Config
+{
+    public class Configuration : IConfiguration
+    {
+        public string GetConnectionString(string name)
+        {
+            string resultConnectionString=String.Empty;
+            string sqlExpression = $"Select ConnectionString from Connections Where Name= '{name}'";
+            string localDbConnectionString;
+            try
+            {
+                localDbConnectionString = ConfigurationManager.
+                    ConnectionStrings["localDbConnection"].ConnectionString;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error config read: {ex.Message}",ex);
+            }
+
+
+            using (var connection = new SqlConnection(localDbConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                        reader.Read();
+
+                        resultConnectionString = reader.GetString(0);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error DB read: {ex.Message}", ex);
+                }
+            }
+            return resultConnectionString;
+        }
+    }
+}
---
+++ Config/IConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordBook.Config
+{
+    public interface IConfiguration
+    {
+        string GetConnectionString(string name);
+    }
+}
